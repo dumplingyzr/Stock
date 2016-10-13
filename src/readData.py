7366b71d2e27aab521102a3140d2c2bcc7685f02
Zhangrui Yao <yaozr@umich.edu>
2016-10-12 21:19:15 -0700

    adding plot data

--- src/readData.py
+++ src/readData.py
@@ -1,11 +1,24 @@
 from yahoo_finance import Share
-import json
+import matplotlib.pyplot as plt
+import pandas as pd
+import os.path
 
-class Quotes:
-    def main(self, symbol):
-        myShare = Share(symbol)
-        historical = myShare.get_historical('1993-10-02','2016-10-12')
-        for i in range (len(historical)):
-            print (historical[i],"\n")
 
-Quotes().main('AAPL')
+class Stock:
+    def __init__(self):
+        self.historical = []
+
+    def get_quote(self, symbol):
+        if not os.path.exists("../stockdata/"+symbol+".csv"):
+            my_share = Share(symbol)
+            pd.DataFrame(my_share.get_historical('1993-10-02','2016-10-12')).to_csv("../stockdata/"+symbol+".csv")
+
+        self.historical = pd.read_csv("../stockdata/"+symbol+".csv")
+
+    def plot(self):
+        self.historical['Adj_Close'].plot(grid=True)
+        plt.show()
+
+myStock = Stock()
+myStock.get_quote('AAPL')
+myStock.plot()
