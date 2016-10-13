f9f7327b118fe8b74339ca7aea9de009af847cd0
Zhangrui Yao <yaozr@umich.edu>
2016-10-12 23:43:39 -0700

    supporting multiple stocks

--- src/readData.py
+++ src/readData.py
@@ -2,23 +2,50 @@ from yahoo_finance import Share
 import matplotlib.pyplot as plt
 import pandas as pd
 import os.path
+import datetime
 
 
 class Stock:
     def __init__(self):
-        self.historical = []
+        self.historical = pd.DataFrame()
 
-    def get_quote(self, symbol):
+    def get_quote(self, symbol, start_date='2000-01-01', end_date=datetime.datetime.now().strftime("%Y-%m-%d")):
         if not os.path.exists("../stockdata/"+symbol+".csv"):
             my_share = Share(symbol)
-            pd.DataFrame(my_share.get_historical('1993-10-02','2016-10-12')).to_csv("../stockdata/"+symbol+".csv")
+            temp = pd.DataFrame(my_share.get_historical(start_date, end_date))
+            if temp.size == 0:
+                return False
+            else:
+                temp.to_csv("../stockdata/"+symbol+".csv")
 
-        self.historical = pd.read_csv("../stockdata/"+symbol+".csv")
+        temp = pd.read_csv("../stockdata/"+symbol+".csv", parse_dates=True,
+                                      index_col="Date", usecols=["Date","Adj_Close"])
+        temp.rename(columns={"Adj_Close": symbol}, inplace=True)
 
-    def plot(self):
-        self.historical['Adj_Close'].plot(grid=True)
+        if self.historical.size == 0:
+            self.historical = temp
+        else:
+            self.historical = self.historical.join(other=temp, how='outer')
+            self.historical.fillna(method="ffill", inplace=True)
+            self.historical.fillna(method="bfill", inplace=True)
+        return True
+
+    def plot(self, symbols=[]):
+        if not symbols:
+            self.historical.plot()
+        else:
+            self.historical[symbols].plot()
         plt.show()
 
-myStock = Stock()
-myStock.get_quote('AAPL')
-myStock.plot()
+
+def test_run():
+    myStock = Stock()
+    myStock.get_quote(symbol='FB')
+    myStock.get_quote(symbol='QCOM')
+    myStock.get_quote(symbol='GOOGL')
+    myStock.get_quote(symbol='VMRI')
+    symbols = ['VMRI']
+    myStock.plot(symbols)
+
+if __name__ == "__main__":
+    test_run()
